310fbac527e80730b090aa9d609b94018a664220
HighImKevin <kevinjohncampbell@gmail.com>
2026-09-22 17:37:09 -0400

    Fix isModernUI check

--- scripts/breadcrumbs.js
+++ scripts/breadcrumbs.js
@@ -472,7 +472,7 @@
     
     // DOM manipulation
     function isModernUI() {
-        return !!document.querySelector('.MuiBox-root') || localStorage.getItem('layout')?.length === 0;
+        return !!document.querySelector('main.MuiBox-root') || localStorage.getItem('layout')?.length === 0;
     }
 
     function resolveBreadcrumbMount() {
